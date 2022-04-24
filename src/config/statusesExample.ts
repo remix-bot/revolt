4721915e74224d34c997ce8300f2fbb0b02d8bee
Allan <88591830+CorpseDev@users.noreply.github.com>
2022-04-24 23:34:27 +0300

    Delete src directory

--- src/config/statusesExample.ts
+++
@@ -1,4 +0,0 @@
-// rename this to statuses.ts and edit as needed
-export const statuses = [
-	{ presence: "Online", text: `%help` }
-];
--- src/statuses.ts
+++
@@ -1,5 +0,0 @@
-// rename this to statuses.ts and edit as needed
-export const statuses = [
-	{ presence: "Online", text: `%help` },
-	{ presence: "Online", text: `%help | Servers: 71 Users: 1817` },
-];

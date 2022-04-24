8e7fe7f42248e8af8a089458b4394da8bcf9eeca
Allan <88591830+CorpseDev@users.noreply.github.com>
2022-04-24 14:32:46 +0300

    Update avatar.ts

--- src/commands/avatar.ts
+++ src/commands/avatar.ts
@@ -2,24 +2,33 @@ import { Message } from "revolt.js/dist/maps/Messages";
 
 export const name = "avatar";
 export const aliases = ["av"];
-export const description =
-	"Returns the first mentioned user's avatar (or if no users are mentioned, the author's).";
+export const description = "Returns the first mentioned user's avatar (or if no users are mentioned, the author's).";
 export const developer = false;
 export const serverOnly = false;
 
-export async function run(msg: Message) {
+export async function run(msg, args, client) {
 	const mentionedUser = msg.mention_ids
 		? msg.client.users.get(msg.mention_ids[0])
 		: null;
-	return msg.channel?.sendMessage(
-		`# ${
-			mentionedUser ? `${mentionedUser.username}'s` : "Your"
-		} avatar:\n\n[**Link**](https://autumn.revolt.chat/avatars/${
-			mentionedUser ? mentionedUser.avatar?._id : msg.author?.avatar?._id
-		}/${
-			mentionedUser
-				? mentionedUser.avatar?.filename
-				: msg.author?.avatar?.filename
-		})`
-	);
+	const avatarUrl = `https://autumn.revolt.chat/avatars/${
+		mentionedUser ? mentionedUser.avatar?._id : msg.author?.avatar?._id
+	}/${
+		mentionedUser
+			? mentionedUser.avatar?.filename
+			: msg.author?.avatar?.filename
+	}`;
+	return msg.channel?.sendMessage({
+		content: `[**Link**](<${avatarUrl}>)`,
+		embeds: [
+			{
+				type: "Text",
+				title: "RexBot",
+				description: `**${
+					mentionedUser ? `${mentionedUser.username}'s` : "Your"
+				} avatar**`,
+				colour: "#e9196c",
+				// media: avatarUrl,
+			},
+		],
+	});
 }

6ffe89e146f4fe84f4914a4285ece55654809051
Fantic <fanticx1@gmail.com>
2022-04-24 03:02:44 +0200

    Delete statuses.ts

--- statuses.ts
+++
@@ -1,5 +0,0 @@
-// rename this to statuses.ts and edit as needed
-export const statuses = [
-	{ presence: "Online", text: `%help` },
-	{ presence: "Online", text: `%help | Servers: 62 Users: 1660` },
-];

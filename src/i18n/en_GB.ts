6d421e6f2d843d0e5ca372ee49f9e3de86ac29ae
Allan <88591830+CorpseDev@users.noreply.github.com>
2022-04-25 14:23:11 +0300

    feat: a whole bunch of stuff

--- src/i18n/en_GB.ts
+++ src/i18n/en_GB.ts
@@ -1,58 +1,54 @@
 export const strings = {
-	archive: {
-		description: "Archives the current channel's messages.",
-		fetchingInfo: "Fetching info...",
-		fetchingMessages: "Fetching messages...",
-		creatingFile: "Creating archive file...",
-		archiveComplete: (channelid: string) => {
-			return `Finished archiving <#${channelid}>! For now, contaxt Rexo for the file.`;
-		},
-	},
-	avatar: {
-		description:
-			"Returns the first mentioned user's avatar (or if no users are mentioned, the author's).",
-	},
-	help: {
-		pingPrefix: (prefix: string) => {
-			return `Hey! My prefix is \`${prefix}\`.`;
-		},
-		noDesc: "No description.",
-	},
-	npm: {
-		npmTitle: (name: string) => {
-			return `${name} on NPM`;
-		},
-		noDesc: "*This library has no description.*",
-		latestVer: "**Latest version**",
-	},
-	ping: {
-		description: "Pong.",
-		pong: "Pong!",
-	},
-	shutdown: {
-		response: "Shutting down...",
-	},
-	wikipedia: {
-		noExtract:
-			"*No extract available - feel free to take a look at the page using the links below*",
-		cannotFindArticle: (input: string) => {
-			return `${input} doesn't seem to be an article - did you spell the title correctly?`;
-		},
-		userAgent:
-			"RexBot/1.0 (https://github.com/rexogamer/rexbot, User:Remagoxer)",
-	},
-	errors: {
-		genericError: "Something went wrong :flushed:",
-		genericErrorWithTrace: (trace: any) => {
-			return `Something went wrong! Please report the following to RexBot's devs:\n\`\`\`js\n${trace}\`\`\``;
-		},
-		couldNotFetchData: "There was an issue fetching the data.",
-		serverOnlyCommand: "This command can only be used in servers.",
-		devOnlyCommand:
-			"This command can only be used by the bot's developers.",
-	},
-	embeds: {
-		accent: "#e9196c)",
-		error: "#e9196c",
-	},
+    archive: {
+        description: "Archives the current channel's messages.",
+        fetchingInfo: "Fetching info...",
+        fetchingMessages: "Fetching messages...",
+        creatingFile: "Creating archive file...",
+        archiveComplete: (channelid) => {
+            return `Finished archiving <#${channelid}>! For now, contaxt Remix for the file.`;
+        },
+    },
+    avatar: {
+        description: "Returns the first mentioned user's avatar (or if no users are mentioned, the author's).",
+    },
+    help: {
+        pingPrefix: (prefix) => {
+            return `Hey! My prefix is \`${prefix}\`.`;
+        },
+        noDesc: "No description.",
+    },
+    npm: {
+        npmTitle: (name) => {
+            return `${name} on NPM`;
+        },
+        noDesc: "*This library has no description.*",
+        latestVer: "**Latest version**",
+    },
+    ping: {
+        description: "Pong.",
+        pong: "Pong!",
+    },
+    shutdown: {
+        response: "Shutting down...",
+    },
+    wikipedia: {
+        noExtract: "*No extract available - feel free to take a look at the page using the links below*",
+        cannotFindArticle: (input) => {
+            return `${input} doesn't seem to be an article - did you spell the title correctly?`;
+        },
+        userAgent: "Remix/1.0 (https://github.com/remix-bot/Remix, User:Remix)",
+    },
+    errors: {
+        genericError: "Something went wrong :flushed:",
+        genericErrorWithTrace: (trace) => {
+            return `Something went wrong! Please report the following to Remix's devs:\n\`\`\`js\n${trace}\`\`\``;
+        },
+        couldNotFetchData: "There was an issue fetching the data.",
+        serverOnlyCommand: "This command can only be used in servers.",
+        devOnlyCommand: "This command can only be used by the bot's developers.",
+    },
+    embeds: {
+        accent: "#e9196c",
+        error: "#e9196c",
+    },
 };

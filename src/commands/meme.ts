8acc07afa1eb96c368cad849053d02a0b303b72d
objectionkink <105684294+objectionkink@users.noreply.github.com>
2022-05-20 13:37:33 -0400

    feat: add subreddits to meme command
    no longer posting from 1 subreddit

--- src/commands/meme.ts
+++ src/commands/meme.ts
@@ -8,7 +8,16 @@ export const developer = false;
 export const serverOnly = false;
 import fetch from 'node-fetch';
 export async function run(msg, args, client) {
-const url = await fetch("https://www.reddit.com/r/meme/random/.json");
+const subreddit = [
+  'dankmemes',
+  'memes',
+  'HolUp',
+  'BlackPeopleTwitter',
+  'comedyhomicide',
+  'SpecialSnowflake'
+]
+const rndSr = subreddit[Math.floor(Math.random()* subreddit.length)] 
+const url = await fetch(`https://www.reddit.com/r/${rndSr}/random/.json`);
       const random = await url.json();
   msg.channel?.sendMessage({
         content: (random[0].data.children[0].data.url),

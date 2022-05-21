2dba95e18e9a61d50830663768be481832c55226
Allan <88591830+CorpseDev@users.noreply.github.com>
2022-05-22 00:09:56 +0300

    feat: no longer using urls just requesting (slap cmd)

--- src/commands/slap.ts
+++ src/commands/slap.ts
@@ -1,4 +1,5 @@
 import { Message } from "revolt.js/dist/maps/Messages";
+import fetch from 'node-fetch';
 
 export const name = "slap";
 export const aliases = ["sendslap"];
@@ -8,31 +9,10 @@ export const developer = false;
 export const serverOnly = false;
 
 export async function run(msg, args, client) {
- var slap = [
-        "https://cdn.nekos.life/slap/slap_001.gif",
-        "https://cdn.nekos.life/slap/slap_002.gif",
-        "https://cdn.nekos.life/slap/slap_003.gif",
-        "https://cdn.nekos.life/slap/slap_004.gif",
-        "https://cdn.nekos.life/slap/slap_005.gif",
-        "https://cdn.nekos.life/slap/slap_006.gif",
-        "https://cdn.nekos.life/slap/slap_007.gif",
-        "https://cdn.nekos.life/slap/slap_008.gif",
-        "https://cdn.nekos.life/slap/slap_009.gif",
-        "https://cdn.nekos.life/slap/slap_010.gif",
-        "https://cdn.nekos.life/slap/slap_011.gif",
-        "https://cdn.nekos.life/slap/slap_012.gif",
-        "https://cdn.nekos.life/slap/slap_013.gif",
-        "https://cdn.nekos.life/slap/slap_014.gif",
-        "https://cdn.nekos.life/slap/slap_015.gif",
-        "https://cdn.nekos.life/slap/slap_016.gif",
-        "https://cdn.nekos.life/slap/slap_017.gif",
-        "https://cdn.nekos.life/slap/slap_018.gif",
-        "https://cdn.nekos.life/slap/slap_019.gif",
-        "https://cdn.nekos.life/slap/slap_020.gif",
-    ];
-      let result = Math.floor((Math.random() * slap.length));
+const url = await fetch('http://api.nekos.fun:8080/api/slap')
+const random = await url.json();
 const mentionedUser = msg.mention_ids ? msg.client.users.get(msg.mention_ids[0]) : null;
-  msg.channel?.sendMessage(`[🖐](${slap[result]}) | Slapping ${mentionedUser ? `${mentionedUser.username}` : "yourself :trol:"}`).catch(e => {
+  msg.channel?.sendMessage(`[🖐](${random.image}) | Slapping ${mentionedUser ? `${mentionedUser.username}` : "yourself :trol:"}`).catch(e => {
   console.error('' + e);
   msg.reply('Something went wrong: 🔒 Missing permission');
     });

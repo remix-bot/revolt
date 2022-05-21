542b0f688a0ce971e40fa0f5a42d2c7c39b437f4
objectionkink <105684294+objectionkink@users.noreply.github.com>
2022-05-21 17:55:08 -0400

    feat: now using nekos.fun because nekos.life blocc
    nekos.life blocked requests for some reason now we using nekos.fun because it works perfectly ::)

--- src/commands/hug.ts
+++ src/commands/hug.ts
@@ -1,4 +1,5 @@
 import { Message } from "revolt.js/dist/maps/Messages";
+import fetch from 'node-fetch';
 
 export const name = "hug";
 export const aliases = ["sendhug"];
@@ -8,31 +9,10 @@ export const developer = false;
 export const serverOnly = false;
 
 export async function run(msg, args, client) {
- var hug = [
-        "https://cdn.nekos.life/hug/hug_001.gif",
-        "https://cdn.nekos.life/hug/hug_002.gif",
-        "https://cdn.nekos.life/hug/hug_003.gif",
-        "https://cdn.nekos.life/hug/hug_004.gif",
-        "https://cdn.nekos.life/hug/hug_005.gif",
-        "https://cdn.nekos.life/hug/hug_006.gif",
-        "https://cdn.nekos.life/hug/hug_007.gif",
-        "https://cdn.nekos.life/hug/hug_008.gif",
-        "https://cdn.nekos.life/hug/hug_009.gif",
-        "https://cdn.nekos.life/hug/hug_010.gif",
-        "https://cdn.nekos.life/hug/hug_011.gif",
-        "https://cdn.nekos.life/hug/hug_012.gif",
-        "https://cdn.nekos.life/hug/hug_013.gif",
-        "https://cdn.nekos.life/hug/hug_014.gif",
-        "https://cdn.nekos.life/hug/hug_015.gif",
-        "https://cdn.nekos.life/hug/hug_016.gif",
-        "https://cdn.nekos.life/hug/hug_017.gif",
-        "https://cdn.nekos.life/hug/hug_018.gif",
-        "https://cdn.nekos.life/hug/hug_019.gif",
-        "https://cdn.nekos.life/hug/hug_020.gif",
-    ];
-      let result = Math.floor((Math.random() * hug.length));
+const url = await fetch('http://api.nekos.fun:8080/api/hug')
+const random = await url.json();
 const mentionedUser = msg.mention_ids ? msg.client.users.get(msg.mention_ids[0]) : null;
-  msg.channel?.sendMessage(`[🤗](${hug[result]}) | Hugging ${mentionedUser ? `${mentionedUser.username}` : "yourself :trol:"}`).catch(e => {
+  msg.channel?.sendMessage(`[🤗](${random.image}) | Hugging ${mentionedUser ? `${mentionedUser.username}` : "yourself :trol:"}`).catch(e => {
   console.error('' + e);
   msg.reply('Something went wrong: 🔒 Missing permission');
     });

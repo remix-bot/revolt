bd1c8fc3f027eb8e9b569850387015ba92f34370
objectionkink <105684294+objectionkink@users.noreply.github.com>
2022-05-22 16:00:44 -0400

    feat: create %pat command

---
+++ src/commands/pat.ts
@@ -0,0 +1,20 @@
+import { Message } from "revolt.js/dist/maps/Messages";
+import fetch from 'node-fetch'
+
+export const name = "pat";
+export const aliases = ["sendpat"];
+export const description = "Pat your homie";
+export const category = "Fun";
+export const developer = false;
+export const serverOnly = false;
+
+export async function run(msg, args, client) {
+const url = await fetch('http://api.nekos.fun:8080/api/pat')
+const random = await url.json();
+const mentionedUser = msg.mention_ids ? msg.client.users.get(msg.mention_ids[0]) : null;
+  msg.channel?.sendMessage(`[:pat:](${random.image}) | Patting ${mentionedUser ? `${mentionedUser.username}` : "yourself :trol:"}`).catch(e => {
+  console.error('' + e);
+  msg.reply('Something went wrong: 🔒 Missing permission');
+    });
+}
+;

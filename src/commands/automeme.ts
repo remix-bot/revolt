d7e71f706321e60740778d5670c829aa64651437
objectionkink <105684294+objectionkink@users.noreply.github.com>
2022-06-03 00:09:53 -0400

    feat: %automeme now uses multi subs

--- src/commands/automeme.ts
+++ src/commands/automeme.ts
@@ -1,33 +1,44 @@
-import { Message } from "revolt.js/dist/maps/Messages";
+import { Message } from 'revolt.js/dist/maps/Messages';
 import got from 'got';
 
-export const name = "automeme";
-export const aliases = ["am"];
-export const description = "Gives you automemes.";
+export const name = 'automeme';
+export const aliases = ['am'];
+export const description = 'Gives you automemes.';
 export const developer = false;
 export const serverOnly = false;
 
 export async function run(msg: Message, args: string[]) {
   let question = args[0];
    if (!question)
-    return msg.reply("I hope you do this from a channel dedicated to Memes, The Memes don't stop! Except for when the bot restarts or shuts down for any apparent reason, use the startup command (`%automeme start.`) again");
-   msg.reply("🔄 **| AutoMeme Starting... (`Please wait 5m`)**")
+    return msg.reply('I hope you do this from a channel dedicated to Memes, The Memes don\'t stop! Except for when the bot restarts or shuts down for any apparent reason, use the startup command (`%automeme start.`) again');
+   msg.reply('🔄 **| AutoMeme Starting... (`Please wait 10m`) testing**')
 
-    setInterval(() => { got("https://www.reddit.com/r/memes/random/.json").then(response => {
-    const [list] = JSON.parse(response.body);
-    const [post] = list.data.children;
+    setInterval(() => {
+      const subreddit = [
+        'dankmemes',
+		    'memes',
+		    'HolUp',
+		    'BlackPeopleTwitter',
+		    'comedyhomicide',
+		    'SpecialSnowflake',
+      ];
+	    const rndSr = subreddit[Math.floor(Math.random() * subreddit.length)];
+      got(`https://www.reddit.com/r/${rndSr}/random/.json`).then(response => {
+        const [list] = JSON.parse(response.body);
+        const [post] = list.data.children;
 
-    const permalink = post.data.permalink;
-    const memeUrl = `https://reddit.com${permalink}`;
-    const memeImage = post.data.url;
-    const memeTitle = post.data.title;
-       msg.channel?.sendMessage({
-            content: (`[${memeTitle}](${memeImage})`),
-        }).catch(e => {
-            console.error('' + e);
+        const permalink = post.data.permalink;
+        const memeUrl = `https://reddit.com${permalink}`;
+        const memeImage = post.data.url;
+        const memeTitle = post.data.title;
+         msg.channel?.sendMessage({
+           content: 
+             (`[${memeTitle}](${memeImage})`),
+         }).catch(e => {
+           console.error('' + e);
             msg.reply('Something went wrong: 🔒 Missing permission');
-        });
-        })
-    }, 300000);
+         });
+      })
+    }, 600000);
 }
 ;

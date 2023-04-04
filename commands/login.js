4d4f14b411882beb293d44c1411b8b23143576e6
ShadowLp174 <samuel.moravec@yahoo.com>
2023-04-04 21:31:20 +0200

    added login mechanics (not done yet)

--- commands/login.js
+++ commands/login.js
@@ -4,10 +4,19 @@ module.exports = {
   command: new CommandBuilder()
     .setName("login")
     .setDescription("Generate a code to connect your remix account with your revolt account.")
+    .addStringOption(o =>
+      o.setName("id")
+        .setDescription("The id you got from logging in at the dashboard.")
+        .setRequired(true))
     .addRequirement(r =>
       r.setOwnerOnly(true)
     ),
-  run: function(msg) {
-    msg.reply("WIP", false);
+  run: async function(msg, data) { // TODO: temporary login (without creating account)
+    const log = data.get("id").value;
+    const verified = await this.loadedModules.get("wb-dashboard").instance.login(log, msg.author);
+    const m = (typeof verified == "string")
+      ? "Login failed! Reason: `" + verified + "`. If this is an error and the issue persist, please contact a team member through the server in my description."
+      : (verified == true) ? "Login succeeded! You can continue to the webpage now." : "An unknown error occured. Please contact a team member if this issue persists!";
+    msg.reply(this.em(m, msg), false);
   }
 }
--- dashboard/index.js
+++ dashboard/index.js
@@ -3,16 +3,88 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const path = require("path");
+const session = require("express-session");
+
+const Josh = require('@joshdb/core');
+const JoshJSON = require('@joshdb/json');
 
 class Dashboard {
   port;
-  constructor(client) {
-    this.port = client.config.webPort || 80;
+  remix;
+  constructor(remix) {
+    this.port = remix.config.webPort || 80;
     server.listen(this.port, () => {
       console.log("Listening on port " + this.port);
     });
+    this.remix = remix;
 
-    app.use(express.static(path.join(__dirname + "/static")))
+    this.db = new Josh({
+      name: 'remix',
+      provider: JoshJSON,
+      providerOptions: {
+        dataDir: path.join(__dirname, "../storage/data")
+      },
+    });
+    this.db.defer.then(async () => {
+      console.log("Database connected. " + (await this.db.size) + " rows loaded.");
+      if (!(await this.db.has("login.tokens"))) {
+        this.db.set("login", { tokens: {}, verified: {} })
+      }
+    });
+
+    app.use(express.json());
+    app.use(express.urlencoded());
+    app.use(express.static(path.join(__dirname, "/static")));
+    app.use(session({
+      secret: this.guid(),
+      resave: false,
+      saveUninitialized: true
+    }));
+
+    app.post("/api/login", async (req, res) => {
+      const user = req.body.userId;
+      req.session.user = user;
+      const id = await this.createLogin(user);
+      res.send(JSON.stringify(id));
+    });
+    app.get("/api/login/verify", async (req, res) => {
+      res.send(JSON.stringify(!!(await this.verifySession(req.session.user))))
+    });
+
+    app.get("/dashboard", async (req, res) => {
+      if (!(await this.verifySession(req.session.user))) return res.status(403).send("Unauthorized");
+      res.sendFile(path.join(__dirname, "/dynamic/dashboard/index.html"));
+    });
+    app.get("/dashboard/style.css", (_req, res) => res.sendFile(path.join(__dirname, "/dynamic/dashboard/style.css")))
+    app.get("/dashboard/index.js", (_req, res) => res.sendFile(path.join(__dirname, "/dynamic/dashboard/index.js")))
+  }
+  guid() {
+    var S4 = function() {
+       return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+    };
+    return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
+  }
+  async verifySession(user) {
+    if (!user) return false;
+    return await this.db.has("login.verified." + user);
+  }
+  async createLogin(id) {
+    if (await this.db.has("login.tokens." + id)) return (await this.db.get("login.tokens." + id)).id;
+    const uid = this.guid();
+    const login = { user: id, id: uid, time: Date.now(), verified: false };
+    this.db.set("login.tokens." + id, login);
+    return uid;
+  }
+  async login(uid, user) {
+    const login = await this.db.get("login.tokens." + user._id);
+    if (!login) return "Unknown user or invalid token!";
+    if (Date.now() - 1000 * 60 * 60 * 6 > login.time) return "Login Expired";
+
+    login.verified = true;
+    await this.db.update("login.tokens." + user._id, login);
+    await this.db.set("login.verified." + user._id, { verified: true });
+
+    return true;
   }
 }
 

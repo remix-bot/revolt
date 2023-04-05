742ffc1c02128b1ceabbd446e483e03dfec62fad
ShadowLp174 <samuel.moravec@yahoo.com>
2023-04-05 10:44:00 +0200

    security fixes (login system)

--- dashboard/index.js
+++ dashboard/index.js
@@ -28,7 +28,7 @@ class Dashboard {
     this.db.defer.then(async () => {
       console.log("Database connected. " + (await this.db.size) + " rows loaded.");
       if (!(await this.db.has("login.tokens"))) {
-        this.db.set("login", { tokens: {}, verified: {} })
+        this.db.set("login", { tokens: {} })
       }
     });
 
@@ -43,16 +43,18 @@ class Dashboard {
 
     app.post("/api/login", async (req, res) => {
       const user = req.body.userId;
+      const id = await this.createLogin(user, req.session);
       req.session.user = user;
-      const id = await this.createLogin(user);
+      req.session.token = id;
       res.send(JSON.stringify(id));
+      // TODO: security risk!
     });
     app.get("/api/login/verify", async (req, res) => {
-      res.send(JSON.stringify(!!(await this.verifySession(req.session.user))))
+      res.send(JSON.stringify(!!(await this.verifySession(req.session))))
     });
 
     app.get("/dashboard", async (req, res) => {
-      if (!(await this.verifySession(req.session.user))) return res.status(403).send("Unauthorized");
+      if (!(await this.verifySession(req.session))) return res.status(403).send("Unauthorized");
       res.sendFile(path.join(__dirname, "/dynamic/dashboard/index.html"));
     });
     app.get("/dashboard/style.css", (_req, res) => res.sendFile(path.join(__dirname, "/dynamic/dashboard/style.css")))
@@ -64,26 +66,31 @@ class Dashboard {
     };
     return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
   }
-  async verifySession(user) {
-    if (!user) return false;
-    return await this.db.has("login.verified." + user);
+  async verifySession(session) {
+    if (!session.user || !session.token) return false;
+    const login = await this.db.get("login.tokens." + session.user);
+    if (!login) return false;
+    return login.verified.some(t => t.id == session.token);
   }
-  async createLogin(id) {
-    if (await this.db.has("login.tokens." + id)) return (await this.db.get("login.tokens." + id)).id;
+  async createLogin(id, session) {
+    if (session.token) return session.token;
+    const existing = await this.db.get("login.tokens." + id);
+    if (!existing) await this.db.set("login.tokens." + id, { user: id, tokens: [], verified: [] });
     const uid = this.guid();
-    const login = { user: id, id: uid, time: Date.now(), verified: false };
-    this.db.set("login.tokens." + id, login);
+    const token = { id: uid, time: Date.now() };
+    this.db.push("login.tokens." + id + ".tokens", token);
     return uid;
   }
   async login(uid, user) {
     const login = await this.db.get("login.tokens." + user._id);
     if (!login) return "Unknown user or invalid token!";
-    if (uid !== login.id) return "Wrong Token";
-    if (Date.now() - 1000 * 60 * 60 * 6 > login.time) return "Login Expired";
+    const idx = login.tokens.findIndex(t => t.id == uid);
+    if (idx == -1) return "Wrong Token";
+    if (Date.now() - 1000 * 60 * 60 * 6 > login.tokens[idx].time) return "Login Expired";
 
-    login.verified = true;
+    const token = login.tokens.splice(idx, 1);
+    login.verified.push(token[0]);
     await this.db.update("login.tokens." + user._id, login);
-    await this.db.set("login.verified." + user._id, { verified: true });
 
     return true;
   }

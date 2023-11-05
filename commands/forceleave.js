7bb4ebd9be2cf3893a80bc9967c6190c06c12136
ShadowLp174 <samuel.moravec@yahoo.com>
2023-11-05 13:09:56 +0100

    added permissions to fl command

--- commands/forceleave.js
+++ commands/forceleave.js
@@ -4,15 +4,17 @@ module.exports = {
   command: new CommandBuilder()
     .setName("forceleave")
     .addAlias("fl")
-    .setDescription("force leave channel without being in it; dev only")
-    .addRequirement(r => r.setOwnerOnly(true))
+    .setDescription("Make Remix leave a channel even if you're not in it.")
+    .addRequirement(r => r.addPermission("ManageChannel"))
+    //.addRequirement(r => r.setOwnerOnly(true))
     .addChannelOption(o =>
       o.setName("channelId")
-        .setDescription("The channel it should leave")
+        .setDescription("The channel that should be left.")
         .setRequired(true)
     ),
   run: async function(msg, data) {
     const cid = data.get("channelId").value;
+    if (msg.channel.server.id !== this.client.channels.get(cid)?.server.id) return msg.reply(this.em("This command has to be run in the same server as the voice channel.", msg), false)
     const p = this.playerMap.get(cid);
     if (!p) return msg.reply(this.em("Player not found", msg));
     if (!p.connection) return msg.reply(this.em("Player not initialized.", msg), false);
